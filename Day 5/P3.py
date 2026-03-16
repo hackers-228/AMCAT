625b2ad1a1c6e9715cfe50d0a928c343d032a833
hackers-228 <shivamburnwal567@gmail.com>
2026-03-16 07:49:46 +0000

    intial commit

--- Day 5/P3.py
+++ Day 5/P3.py
@@ -1,5 +1,6 @@
 def add():
     val1 = int(input("enter first value:"))
     val2 = int(input("enter second value:"))
-    print(val1+val2)
-add()
+    # print(val1+val2)
+    return val1+val2
+print(add())
